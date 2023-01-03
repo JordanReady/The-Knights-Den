535983c98ed202bef7ef3e10d4c89e0c696a4e48
JordanReady <88412345+JordanReady@users.noreply.github.com>
2023-01-03 23:43:34 +0000

    stats change

--- app/javascript/src/stats/stats.jsx
+++ app/javascript/src/stats/stats.jsx
@@ -15,8 +15,10 @@ function Stats() {
           <div className="row">
             <div className="col-12 col-md-9 mx-auto my-4">
               <div className="stats-box p-4">
-                <h2 className="mb-4 header">Stats</h2>
                 <div className="row">
+                  <div className="col-12">
+                    <h2 className="mb-4 username shadow">Username</h2>
+                  </div>
                   <div className="col-12 col-md-3">
                     <h3 className="stat-header">Games</h3>
                     <h3 className="stat-num">0</h3>
